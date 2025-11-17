5b2bc05aa22774768e33ab85a557a2f97593de75
Manny Bennett <113947260+mannybennett@users.noreply.github.com>
2025-11-17 20:03:53 +0000

    server swap + initial winner logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -6,12 +6,31 @@ const [player1, _setPlayer1] = useState<string>('Player 1');
 const [player2, _setPlayer2] = useState<string>('Player 2');
 const [player3, _setPlayer3] = useState<string>('Player 3');
 const [player4, _setPlayer4] = useState<string>('Player 4');
-const [score, setScore] = useState<number[]>([0, 0, 2]);
-const [turn, setTurn] = useState<number>(1);
-const [_pointWinner, setPointWinner] = useState<number>(1);
+const [score, setScore] = useState<number[]>([0, 0, 2]); // [Team1, Team2, Server]
+const [turn, setTurn] = useState<number>(1); // 1 = Team1, 2 = Team2
+const [_pointWinner, setPointWinner] = useState<number>(1); // 1 = Team1, 2 = Team2
+const [gameWinner, setGameWinner] = useState<string>('Team');
+const [winnerDisplay, setWinnerDisplay] = useState<string>('none');
+const [courtDisplay, setCourtDisplay] = useState<string>('flex');
+
+useEffect(() => {
+  if (score[0] === 11 || score[1] === 11) {
+    handleGameWinner();
+  };
+}, [score]);
+
+const handleGameWinner = (): void => {
+  if (score[0] === 11) {
+    setGameWinner('Team 1');
+  } else {
+    setGameWinner('Team 2');
+  };
+  setCourtDisplay('none');
+  setWinnerDisplay('flex');
+};
 
 const handleServer = (): void => {
-  console.log('Switch server number')
+  console.log('Switch server')
   setScore(prev => {
     const newServer = [...prev];
     newServer[2] === 1 ? newServer[2] = 2 : newServer[2] = 1;
@@ -61,24 +80,39 @@ const handleReset = (): void => {
   setScore([0, 0, 2]);
   setPointWinner(1);
   setTurn(1);
+  setWinnerDisplay('none');
+  setCourtDisplay('flex');
+  setGameWinner('Team');
 };
 
   return (
     <main>
       <div id='container'>
-        <div id='court'>
+        <div id='winner' style={{ display: winnerDisplay }}>
+          <h2>WINNER</h2>
+          <h1>{gameWinner}</h1>
+        </div>
+        <div id='court' style={{ display: courtDisplay }}>
           <section id='top' onClick={handleTeam2Score}>
             <div className='services'>
-              <div className='service1'>{player3}</div>
-              <div className='service2'>{player4}</div>
+              <div className='service1'>
+                {`${score[1] % 2 === 0 ? player4 : player3}`}
+              </div>
+              <div className='service2'>
+                {`${score[1] % 2 === 0 ? player3 : player4}`}
+              </div>
             </div>
             <div id='kitchen1'>Team 2</div>
           </section>
           <section id='bottom' onClick={handleTeam1Score}>
             <div id='kitchen2'>Team 1</div>
             <div className='services'>
-              <div className='service1'>{player2}</div>
-              <div className='service2'>{player1}</div>
+              <div className='service1'>
+                {`${score[0] % 2 === 0 ? player2 : player1}`}
+              </div>
+              <div className='service2'>
+                {`${score[0] % 2 === 0 ? player1 : player2}`}
+              </div>
             </div>
           </section>
         </div>
